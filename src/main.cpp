9c71a3f2ae542db74895d1a7879d54e7445ce4eb
Khoh Zong Eu <khohzongeu@gmail.com>
2026-04-30 23:23:33 +1000

    Removed redundant events.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,14 @@
 #include <TFT.h>  
 #include <SPI.h>
 
+#include "events.h"
+
 const int CS =  10;
 const int DC =  9;
 const int RST = 8;
 
 TFT TFTscreen = TFT(CS, DC, RST);
 
-#include "events.h"
 
 events event;
 
